fac897224a65569c8418f7d6bb41e4b308ff3019
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-05-08 16:15:43 +0700

    fix: completion is not load correctly

--- app/rust.sh
+++ app/rust.sh
@@ -8,9 +8,14 @@ export RUSTPATH="${__RUSTPATH:-$HOME/.cargo}"
 if _myzs:internal:checker:folder-exist "$RUSTPATH"; then
   _myzs:internal:path-append "$RUSTPATH/bin"
 
-  export RUSTFPATH="/usr/share/zsh/site-functions/_rustup"
-  if ! _myzs:internal:checker:file-exist "$RUSTFPATH"; then
-    rustup completions zsh >"$RUSTFPATH"
+  RUSTFPATH="$RUSTPATH/completions"
+  if ! _myzs:internal:checker:folder-exist "$RUSTFPATH"; then
+    mkdir -p "$RUSTFPATH"
+  fi
+
+  export __RUSTFPATH="$RUSTFPATH/_rustup"
+  if ! _myzs:internal:checker:file-exist "$__RUSTFPATH"; then
+    rustup completions zsh >"$__RUSTFPATH"
   fi
 
   _myzs:internal:fpath-push "$RUSTFPATH"
